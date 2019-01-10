ada666cca8da933f451b077bff9e8aab5e521511
MonikaGanko <monikaganko@gmail.com>
2019-01-10 23:42:17 +0100

    Registration, login, account activation by e-mail

---
+++ src/main/java/pl/coderslab/models/User.java
@@ -0,0 +1,54 @@
+package pl.coderslab.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+import org.hibernate.annotations.Type;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    private UUID userUUID = UUID.randomUUID();
+
+    @NotBlank
+    private String firstname;
+
+    @NotBlank
+    private String lastname;
+
+    @NotBlank
+    private String password;
+
+    @Transient
+    private String confirmPassword;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    private boolean enabled = true;
+
+    private boolean validated = true;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+}
