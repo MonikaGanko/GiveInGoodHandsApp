9befbb4aa3eb8cf573916b1fc7a89fc44527fab7
MonikaGanko <monikaganko@gmail.com>
2019-01-25 22:33:29 +0100

    Admin controller updated

--- src/main/java/pl/coderslab/controllers/AdminController.java
+++ src/main/java/pl/coderslab/controllers/AdminController.java
@@ -15,6 +15,7 @@ import pl.coderslab.services.UserService;
 
 import javax.validation.Valid;
 import java.util.Currency;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -32,6 +33,11 @@ public class AdminController {
         return "admin/adminsManagement";
     }
 
+    @RequestMapping("/usersManagement")
+    public String getUsersPage() {
+        return "admin/usersManagement";
+    }
+
     @GetMapping("/adminsList")
     public String getAdminsList(Model model) {
         Role adminRole = (Role) roleService.findByRole("ROLE_ADMIN");
@@ -43,34 +49,20 @@ public class AdminController {
     @GetMapping("/delete/{id}")
     public String delete(@AuthenticationPrincipal CurrentUser currentUser, @PathVariable Long id, Model model) {
         boolean isAdmin = false;
-        //boolean isUser = false;
         User entityUser = currentUser.getUser();
         Set<Role> userRoles = entityUser.getRoles();
         Role adminRole = (Role) roleService.findByRole("ROLE_ADMIN");
-        //Role userRole = (Role) roleService.findByRole("ROLE_USER");
-         /*   if (entityUser.getRoles().equals(adminRole.getRole())) {
-                userService.deleteUser(userService.findById(id));
-                return "redirect:../adminsList";
-            }
-            return "/admin/dashboard";
-*/
 
         for (Role r : userRoles) {
             if (r.getRole().equals(adminRole.getRole())) {
                 isAdmin = true;
                 break;
-                // } else if (r.getRole().equals(userRole.getRole())) {
-                //     isUser = true;
-                //    break;
             }
         }
         if (isAdmin) {
             userService.deleteUser(userService.findById(id));
             model.addAttribute("entityUser", entityUser);
             return "redirect:../adminsList";
-/*        } else if (isUser) {
-            return "user/dashboard";*/
-            //  }
         } else {
             return "403";
         }
@@ -97,10 +89,15 @@ public class AdminController {
             model.addAttribute("admin", admin);
             return "admin/addAdmin";
         } else {
-            userService.saveUser(admin);
-           /* Long newAdminId = admin.getId();
-            Long adminRoleId = roleService.findIdByRole("ROLE_ADMIN");
-            admin.setRoles();*/
+           /* Set <Role> roles = admin.getRoles();
+            if (roles==null){
+                roles = new HashSet<>();
+            }
+            Role adminRole = roleService.findByRole("ROLE_ADMIN");
+            roles.add(adminRole);
+            admin.setRoles(roles);*/
+            userService.saveAdmin(admin);
+
             return "redirect: /../adminsList";
         }
     }
@@ -108,7 +105,7 @@ public class AdminController {
     @GetMapping("/adminIdCheck/{id}")
     public String checkAdminId(@AuthenticationPrincipal CurrentUser currentUser, Model model, @PathVariable Long id) {
         User entityUser = currentUser.getUser();
-        if (entityUser.getId() == id) {
+        if (entityUser.getId().equals(id)) {
             Role adminRole = (Role) roleService.findByRole("ROLE_ADMIN");
             List<User> admins = userService.findAllByRoles(adminRole);
             model.addAttribute("admins", admins);
@@ -117,5 +114,39 @@ public class AdminController {
         }
         return "redirect: /../../delete/" + id;
     }
+
+
+    @GetMapping("/update/{id}")
+    public String update(@AuthenticationPrincipal CurrentUser currentUser, @PathVariable Long id, Model model) {
+        boolean isAdmin = false;
+        User entityUser = currentUser.getUser();
+        Set<Role> userRoles = entityUser.getRoles();
+        Role adminRole = (Role) roleService.findByRole("ROLE_ADMIN");
+
+        for (Role r : userRoles) {
+            if (r.getRole().equals(adminRole.getRole())) {
+                isAdmin = true;
+                break;
+            }
+        }
+        if (isAdmin) {
+            userService.updateUser(userService.findById(id));
+            User admin = userService.findById(id);
+            model.addAttribute("admin", admin);
+            return "admin/updateAdmin";
+        } else {
+            return "403";
+        }
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(@PathVariable Long id, @ModelAttribute @Valid User admin, BindingResult result) {
+        if (result.hasErrors()) {
+            return "/admin/updateAdmin";
+        }
+        userService.updateUser(admin);
+        return "redirect:../adminsList";
+    }
+
 
 }
